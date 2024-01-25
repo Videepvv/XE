5f4bd6321f63b4f0d42d18237a23e7a049ddd83d
Videep <videepvv@gmail.com>
2024-01-25 10:58:19 -0700

    added dataset

--- pipeLine.py
+++ pipeLine.py
@@ -5,33 +5,42 @@ import torch
 device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 print("Using device:", device)
 # Initialize CrossEncoder
-model = CrossEncoder(is_training=False, model_name='allenai/longformer-base-4096').to(device)
+model = CrossEncoder(is_training=True, model_name='allenai/longformer-base-4096').to(device)
 
 utt = UtteranceEncoding()
 
 print('This is the list of props' ,(utt.props))
-# Example sentences
+# Example sentence
+'''
 highest_score = 0
 best_proposition = None
+counter = 0
 for currentProposition in utt.props:
-    sentence1 = "And we will tell you that this red cube on top is ten grams"
-    sentence2 =  currentProposition
+    if counter == 20:
+        break
+    counter+=1
+    statement = "And we can tell you that that red cube right there is ten grams yep and you're going you're going to flip the piece of paper um"
+    proposition = "red block equals ten"
+    #sentence2 =  currentProposition
 
 # Combine and tokenize sentences
     input_text = sentence1 + " [SEP] " + sentence2
+
+    print(input_text)
     inputs = model.tokenizer.encode(input_text, return_tensors="pt").to(device)
 
 #attention_mask = model.tokenizer.get_attention_mask(inputs)
-    print(inputs)
-# Get similarity score by explicitly calling the forward method
+    #print(inputs)
+# Get similarity score
     similarity_score = model.forward(input_ids=inputs, attention_mask=None)
     score = similarity_score.item()
-
+    print(score)
 # Check if this score is the highest
-    if score > highest_score:
-        highest_score = score
-        best_proposition = currentProposition
+    #if score > highest_score:
+    #    highest_score = score
+    #    best_proposition = currentProposition
 
 print("Best Proposition:", best_proposition)
 print("Highest Similarity Score:", highest_score)
+'''
 
